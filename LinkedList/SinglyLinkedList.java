26f3ed46610ea117c3cf60d336b0ec4de0604110
Arundhati Bandopadhyaya <bandopadhyaya.a@northeastern.edu>
2025-01-31 12:48:22 -0500

    insertAtHead() and printList()

--- LinkedList/SinglyLinkedList.java
+++ LinkedList/SinglyLinkedList.java
@@ -17,4 +17,36 @@ public class SinglyLinkedList {
     public SinglyLinkedList(){
         this.head = null;
     }
+
+    public void insertAtHead(int data){
+        if (head == null){
+            this.head = new Node(data);
+            return;
+        }
+
+        Node current = new Node(data);
+        current.next = head;
+        this.head = current;
+    }
+
+    public void printList(){
+        if (head == null) return;
+
+        Node current = head;
+
+        while (current != null){
+            System.out.print(current.data+" ");
+            current = current.next;
+        }
+    }
+
+    public static void main(String[] args){
+        
+        SinglyLinkedList SLL = new SinglyLinkedList();
+        SLL.insertAtHead(5);
+        SLL.insertAtHead(10);
+        SLL.insertAtHead(15);
+        SLL.insertAtHead(20);
+        SLL.printList();
+    }
 }

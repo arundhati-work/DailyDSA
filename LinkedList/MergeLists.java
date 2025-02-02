d8a377aeecfaa1e15c4936c9ffedfc42141a9886
Arundhati Bandopadhyaya <bandopadhyaya.a@northeastern.edu>
2025-02-01 22:58:34 -0500

    merge iterative update

--- LinkedList/MergeLists.java
+++ LinkedList/MergeLists.java
@@ -39,16 +39,7 @@ public class MergeLists {
             }
         }
 
-        while (list1 != null){
-            dummy.next = list1;
-            dummy = dummy.next;
-            list1 = list1.next;
-        }
-        while (list2 != null){
-            dummy.next = list2;
-            dummy = dummy.next;
-            list2 = list2.next;
-        }
+        dummy.next = (list1 != null)? list1 : list2;
         return newHead;
 
     }

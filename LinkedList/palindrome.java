138c48d9538adda715d29a0aa2d67f7ec9c94ae8
Arundhati Bandopadhyaya <bandopadhyaya.a@northeastern.edu>
2025-02-05 12:34:05 -0500

    palindrome with reverse and compare approach

--- LinkedList/palindrome.java
+++ LinkedList/palindrome.java
@@ -6,53 +6,49 @@ public class palindrome {
     public ListNode middle(ListNode head){
         if (head == null || head.next == null) return head;
 
+        ListNode prev = null;
         ListNode tortoise = head;
         ListNode hare = head;
 
-        while (hare.next != null && hare.next.next != null){
+        while (hare != null && hare.next != null){
+            prev = tortoise;
             tortoise = tortoise.next;
             hare = hare.next.next;
         }
+
+        if (prev!= null) prev.next = null;
+
         return tortoise;
     }
 
     public ListNode reverse(ListNode head){
-        
         if (head == null || head.next == null) return head;
-
         ListNode prev = null;
-        ListNode current = head;
+        ListNode curr = head;
         ListNode post = head.next;
 
-        while (current != null){
-            current.next = prev;
-            prev = current;
-            current = post;
-            if (post!= null) post = post.next;
+        while (curr != null){
+            curr.next = prev;
+            prev = curr;
+            curr = post;
+            if (post != null) post = post.next;
         }
+
         return prev;
     }
-
 
     public boolean isPalindrome(ListNode head) {
         if (head == null || head.next == null) return true;
 
-        ListNode midElement = middle(head);
+        ListNode mid = middle(head);
+        ListNode rev = reverse(mid);
 
-        ListNode elementAfterMid = midElement.next;
-        midElement.next = null;
-        ListNode rev2 = reverse(elementAfterMid);
-
-        ListNode current = head;
-
-        while (current!= null && rev2 != null){
-            if (current.val == rev2.val){
-                current = current.next;
-                rev2 = rev2.next;
-            } else{
-                return false;
-            }
+        while (head!= null && rev != null){
+            if (head.val != rev.val) return false;
+            head = head.next;
+            rev = rev.next;
         }
+
         return true;
     }
 }
